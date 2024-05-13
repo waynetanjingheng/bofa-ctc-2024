ac17430bfd46a9c118916c31710e6f1eea5d6c3f
waynetanjingheng <wtjh00@gmail.com>
2024-05-13 11:33:44 +0800

    WIP: matching algo

--- backend/order_queue.py
+++ backend/order_queue.py
@@ -1,4 +1,9 @@
 from heapq import *
+from datetime import datetime as dt
+
+
+def _format_time(arrival_time):
+    return dt.strptime(arrival_time, "%H:%M:%S").time()
 
 
 class Order:
@@ -6,7 +11,7 @@ class Order:
         self.order_id = order_id
         self.client_id = client_id
         self.rating = rating
-        self.arrival_time = arrival_time
+        self.arrival_time = _format_time(arrival_time)
         self.price = price
         self.quantity = quantity
 
@@ -30,7 +35,7 @@ class Order:
         )
 
     def __repr__(self):
-        return f"{self.order_id, self.client_id, self.rating, self.arrival_time, self.price, self.quantity}"
+        return f"{self.order_id, self.client_id, self.rating, self.arrival_time.strftime('%H:%M:%S'), self.price, self.quantity}"
 
 
 class OrderQueue:
@@ -40,7 +45,21 @@ class OrderQueue:
         heapify(self.bid_heap)
         heapify(self.ask_heap)
 
+    def get_bid_count(self):
+        return len(self.bid_heap)
+
+    def get_ask_count(self):
+        return len(self.ask_heap)
+
+    def add_bid_order(self, new_bid):
+        heappush(self.bid_heap, new_bid)
+
+    def add_ask_order(self, new_ask):
+        heappush(self.ask_heap, new_ask)
+
     def match(self):
+        matches = 0
+        
         while self.bid_heap:
             best_bid = heappop(self.bid_heap)
             print(best_bid)
---
+++ backend/read_csv.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from dotenv import load_dotenv
+import os
+from order_queue import Order
+
+load_dotenv()
+
+EXAMPLE_INPUT_ORDERS_PATH = os.environ.get("EXAMPLE_INPUT_ORDERS_PATH")
+EXAMPLE_INPUT_CLIENTS_PATH = os.environ.get("EXAMPLE_INPUT_CLIENTS_PATH")
+
+order_df = pd.read_csv(EXAMPLE_INPUT_ORDERS_PATH)
+client_df = pd.read_csv(EXAMPLE_INPUT_CLIENTS_PATH)
+
+bids = []
+asks = []
+
+# Convert orders into Order objects and store them according to side
+
+""" 
+        self.order_id = order_id
+        self.client_id = client_id
+        self.rating = rating
+        self.arrival_time = arrival_time
+        self.price = price
+        self.quantity = quantity
+"""
+
+# Generate hashmap of client ratings
+
+CLIENT_RATINGS = {
+    client["ClientID"]: client["Rating"] for _, client in client_df.iterrows()
+}
+
+for _, order in order_df.iterrows():
+    new_order = Order(
+        order_id=order["OrderID"],
+        client_id=order["Client"],
+        rating=CLIENT_RATINGS[order["Client"]],
+        arrival_time=order["Time"],
+        price=order["Price"],
+        quantity=order["Quantity"],
+    )
+    if order["Side"] == "Buy":
+        bids.append(new_order)
+    else:
+        asks.append(new_order)
+
+# print(bids)
+print(asks)
