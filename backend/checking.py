d5ddf9c2936f19909c2d17f07ded3fd1a73af58a
Emmelyn Kek <105198600+emmelynkek@users.noreply.github.com>
2024-05-13 13:45:26 +0800

    update

--- backend/checking.py
+++ backend/checking.py
@@ -14,39 +14,42 @@ finalJoin = pd.merge( initialJoin, instruments, how='left', left_on="Instrument"
 
 finalJoin.drop(columns='Client', inplace=True)  
 finalJoin.drop(columns='Instrument', inplace=True)  
-failedTrades = pd.DataFrame(columns=['REASON', 'ClientID','Currencies','PositionCheck','Rating','Time','OrderID','Quantity', 'Side', 'InstrumentID', 'Currency', 'LotSize', 'completed_buy'])
-
+failedTrades = pd.DataFrame(columns=['REASON', 'ClientID','Currencies','PositionCheck','Rating','Time','OrderID','Quantity', 'Side', 'InstrumentID', 'Currency', 'LotSize'])
 
 passedTrades = pd.DataFrame(columns=['ClientID','Rating','Time','OrderID','Quantity', 'Side', 'InstrumentID', 'Currency', 'LotSize'])
 completed_buys ={} # hash map storing (client, stock): quantity
 
 
+#0. Do for each row in the dataframe
 
+
+#1. Checking
 
 def checking(row): 
     if pd.isna(row['InstrumentID']) : 
         row['REASON'] = "REJECTED-INSTRUMENT NOT FOUND"
         failedTrades.loc[len(failedTrades)] =row
-        
-     
+        return False
 
     currencySet = set(row['Currencies'].split(',')) if isinstance(row['Currencies'], str) else set()
     # Check currency presence
     if row['Currency'] not in currencySet:
         row['REASON'] = "REJECTED-MISMATCH CURRENCY"
         failedTrades.loc[len(failedTrades)] =row
+        return False
         
 
-    # check lot size 
+    # Check lot size 
     if row['Quantity']% row['LotSize'] != 0: 
         row['REASON'] = "REJECTED-INVALID LOT SIZE"
         failedTrades.loc[len(failedTrades)] =row
+        return False
         
 
     completedBuys = row["completed_buy"]
     length = len(completedBuys)
 
-    # check posiiton
+    # Check position
     if row['PositionCheck']=="Y" and row['Side'] == "Sell": 
         Instrument= row['InstrumentID']    
         Client = row['ClientID']
@@ -62,15 +65,28 @@ def checking(row):
                 
                 row['REASON'] = "REJECTED-POSITION CHECK FAILED"
                 failedTrades.loc[len(failedTrades)] =row
+                return False
                 
         else: 
            
             row['REASON'] = "REJECTED-POSITION CHECK FAILED"
             failedTrades.loc[len(failedTrades)] =row
+            return False
         
         passedTrades.loc[len(passedTrades)] =row
+
+        return True
             
-        
+
+#2. Matching algo       
+
+# go into hashmap for instrument and currency which stores the 2 heaps
+# if any sell => look at buy
+#   - add sell to heap 
+#   - pop sell and compare to buy until invalid 
+# if any buy => look at sell
+
+
 
     
     
